3650b51d5427fed9cba4ec164cacbdb2cd0affd4
Huy Hoang <huy2000kudo@gmail.com>
2024-03-25 23:24:05 +0700

    add

--- src/util/_DATA.test.js
+++ src/util/_DATA.test.js
@@ -1,4 +1,4 @@
-const { _saveQuestionAnswer } = require("./_DATA");
+const { _saveQuestion, _saveQuestionAnswer } = require("./_DATA");
 describe("_saveQuestionAnswer", () => {
   it("should return true for correct parameters", async () => {
     const response = await _saveQuestionAnswer({
@@ -20,3 +20,37 @@ describe("_saveQuestionAnswer", () => {
     expect(response).toBe("Please provide authedUser, qid, and answer");
   });
 });
+
+describe("_saveQuestion", () => {
+  test("returns the saved question with all expected fields populated when correctly formatted data is passed to the function", async () => {
+    const mockQuestion = {
+      author: "name of author",
+      optionOneText: "option one",
+      optionTwoText: "option two",
+    };
+
+    const actual = await _saveQuestion(mockQuestion);
+    const { author, optionOne, optionTwo } = actual;
+    const text1 = optionOne.text;
+    const text2 = optionTwo.text;
+
+    expect(author).toBe("name of author");
+    expect(text1).toBe("option one");
+    expect(text2).toBe("option two");
+  });
+});
+
+describe("_saveQuestion", () => {
+  it("returns an error when incorrect data is passed to the function", async () => {
+    const mockQuestion = {
+      author: null,
+      optionOneText: null,
+      optionTwoText: null,
+    };
+
+    await expect(_saveQuestion(mockQuestion)).rejects.toEqual(
+      "Please provide optionOneText, optionTwoText, and author"
+    );
+  });
+});
+
